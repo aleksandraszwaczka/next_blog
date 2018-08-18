67db5bea92fc194f4532ec93d35f7b86864369c1
Aleksandra Szwaczka <aleksandra.szwaczka@gmail.com>
2018-08-18 15:58:30 +0200

    create articles create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,24 @@
 class ArticlesController < ApplicationController
   def index
-    
+
+  end
+
+  def new
+
+  end
+
+  def create
+    articles_params = params.require(:article).permit(:title, :text)
+
+    @article = Article.new(articles_params)
+    @article.save
+
+    redirect_to articles_path
+    #ścieżki do tego gdzie przekierować szukamy za pomocą komendy bin/rails router w konsoli
+
+
+    # render plain: params[:articles].permit(:title, :text).inspect
+    #chcemy wyrenderować zwykły tekst, który podajemy po plain, ale tu wpisujemy w to miejsce parametry
+    #.permit definiuje dopuszczalne parametry do wywołania, to chroni np hasła
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   get 'articles', to: 'articles#index'
   #jeśli wpiszesz po adresie głównym w przeglądarce 'articles' to on przejdzie do 'articles#index'
+  get 'articles/new', to: 'articles#new', as: 'new_article'
+  #as: zmienia nazwę prefixu
+  post 'articles', to: 'articles#create'
+  #get czyta dane (tylko odczytuje)
+  #post tworzy nowe zasoby
+  #put aktualizuje zasoby
+  #delete jest do usuwania zasbów, które istnieją
 end
