09e6df1745ae862aa218244e85c2f4dbb3900dcf
Aleksandra Szwaczka <aleksandra.szwaczka@gmail.com>
2018-09-01 15:22:26 +0200

    up and down option

--- db/migrate/20180901125838_add_not_null_constrainnts_in_coments.rb
+++ db/migrate/20180901125838_add_not_null_constrainnts_in_coments.rb
@@ -1,6 +1,11 @@
 class AddNotNullConstrainntsInComents < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_column_null :comments, :commenter, false
     change_column_null :comments, :body, false
+  end
+
+  def down
+    change_column_null :comments, :commenter, true
+    change_column_null :comments, :body, true
   end
 end
