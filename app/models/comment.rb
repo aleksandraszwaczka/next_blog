80fa3ddf5dbb30e31b1eed8c7f68439734007e26
Aleksandra Szwaczka <aleksandra.szwaczka@gmail.com>
2018-09-01 14:22:51 +0200

    valid email

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
+  EMAIL_REGEXP = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   belongs_to :article
 
   validates :commenter, presence: true
+  validate :validate_comenter
   validates :body, presence: true, length: { in: 6..500 }
+
+  def validate_comenter
+    errors.add(:commenter, :invalid) unless commenter=~ EMAIL_REGEXP
+  end
 end
